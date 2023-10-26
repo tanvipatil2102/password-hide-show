cc61f68b0c2dc2b54d144b99125fdf80509b4742
tanvipatil2102 <143738577+tanvipatil2102@users.noreply.github.com>
2023-10-26 21:29:06 +0530

    work done

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,20 @@ const icon1 = document.getElementById("icon1");
 const icon2 = document.getElementById("icon2");
 const inputControl = document.getElementById("inputControl");
 
+let flag = true;
 
 
 const onClickHandler = (eve)=>{
-   let getAtr = inputControl.setAttribute("type", "text");
-   cl(getAtr);
-   icon1.classList.add("d-none");
-   icon2.classList.remove("d-none");
+   if(flag){
+      inputControl.setAttribute("type", "text");
+   }else{
+      inputControl.setAttribute("type", "password");
+   }
+  
+   icon1.classList.toggle("fa-eye");
+   icon1.classList.toggle("fa-eye-slash");
+
+   flag = !flag;
 }
 
-const onClickRemove = (eve)=>{
-    let getAtr = inputControl.setAttribute("type", "password");
-    cl(getAtr);
-    icon1.classList.remove("d-none");
-    icon2.classList.add("d-none");
- }
-
-
 icon1.addEventListener("click", onClickHandler);
-icon2.addEventListener("click", onClickRemove);
